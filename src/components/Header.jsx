9f60c6b6f2496c222126dd462193f351f71ce57a
fantasticfoo <102485361+fantasticfoo@users.noreply.github.com>
2024-07-28 15:42:31 -0700

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FlareIcon from "@mui/icons-material/Flare";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <FlareIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
